9c680a92121a9d168a0e06c6c6dba424523c1a93
KyleTime <kylethompsonburnett@gmail.com>
2023-07-07 11:29:33 -0700

    added scare functionality

--- Assets/Resources/Scripts/GoofyMeter.cs
+++ Assets/Resources/Scripts/GoofyMeter.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class GoofyMeter : MonoBehaviour
 {
+    public static GoofyMeter inst;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        inst = this;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Scare(float power)
+    {
+
     }
 }
